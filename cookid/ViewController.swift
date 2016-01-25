e9eb889d04568a2b84bd18f14cdd25575922fec5
Utilisateur invité <Guest@MacBook-Air-de-SUPInternet-9.local>
2016-01-25 15:04:12 +0100

    update storyboard

---
+++ cookid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cookid
+//
+//  Created by Utilisateur invité on 19/01/2016.
+//  Copyright © 2016 Utilisateur invité. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+       
+
+}
+
---
+++ cookidTests/cookidTests.swift
@@ -0,0 +1,36 @@
+//
+//  cookidTests.swift
+//  cookidTests
+//
+//  Created by Utilisateur invité on 19/01/2016.
+//  Copyright © 2016 Utilisateur invité. All rights reserved.
+//
+
+import XCTest
+@testable import cookid
+
+class cookidTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
